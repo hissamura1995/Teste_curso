5023e2ee8483127bd635432fb1b1d0b80e1afaf6
Renan Hissamura <renanhissamura@hotmail.com>
2021-03-17 22:25:21 -0300

    Projeto inicido, mostrando a mensagem Hello World

---
+++ EstudosURI2/Classe/Funcioario.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace EstudosURI2.Classe
+{
+    class Funcioario
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+
+        public Funcioario(int id, string name, double salary)
+        {
+            ID = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public void IncreaseSalary(double percent)
+        {
+            Salary += Salary * percent / 100;
+        }
+
+        public override string ToString()
+        {
+            return ID + ", " + Name + ", " + Salary;
+        }
+    }
+}
+
---
+++ EstudosURI2/Point.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EstudosURI2
+{
+    struct Point
+    {
+        public double X;
+        public double Y;
+
+        public override string ToString()
+        {
+            return "(" + X + ", " + Y + ")";
+        }
+    }
+}
